73040b4e8cd8891573b2384c848a16263af080c9
EmilyEdna <847432003@qq.com>
2021-03-31 17:45:18 +0800

    1

---
+++ SS-SSR-V2RAY/Const/Header.py
@@ -0,0 +1,4 @@
+import Const
+
+Const.Agent_K='User-Agent'
+Const.Agent_V='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'
--- SS-SSR-V2RAY/V2RAY/Scrapy_V2ray.py
+++ SS-SSR-V2RAY/V2RAY/Scrapy_V2ray.py
@@ -8,7 +8,7 @@ class V2RAY(object):
 
     def GetHtmlText(self):
         vmess = []
-        SSURL = "https://free.ishadowx.biz/"
+        SSURL = "https://my.ishadowx.biz"
         headers = {
             'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
